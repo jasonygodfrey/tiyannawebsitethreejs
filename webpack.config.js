0300f2d549057b463dac1346dc3418e16bd88b5e
Jason <83245646+jasonygodfrey@users.noreply.github.com>
2023-04-15 02:59:53 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const entry = {};
+
+module.exports = {
+    mode: "development", //'production', //
+    entry: './js/main.js',
+    output: {
+        filename: 'main.min.js',
+        path: __dirname + "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [['@babel/preset-env', { modules: false }]]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(vert|frag|obj)$/i,
+                use: 'raw-loader',
+            }
+        ],
+    },
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [
+    //         new TerserPlugin({
+    //             test: /\.js(\?.*)?$/i,
+    //         })
+    //     ],
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: false,
+        open: true,
+        disableHostCheck: true
+    }
+};
